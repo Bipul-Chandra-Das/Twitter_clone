63228b6eb7c26b4ad5d2fe3fef1a3629fe3f39ea
Bipul-Chandra-Das <ayyashadnaan@gmail.com>
2026-08-29 14:23:49 +0600

    encapsulation assignment

---
+++ twitterClone/twitterClone.Domain/tweet.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace twitterClone.Domain.Entities
+
+{
+    public class Tweet
+    {
+        private Guid _id;
+        private Guid _authorId;
+        private string _content;
+        private bool _isDeleted;
+
+        public Tweet(Guid authorId, string content)
+        {
+            _id = Guid.NewGuid();
+            _authorId = authorId;
+            _content = content;
+            _isDeleted = false;
+        }
+
+        public Guid ID
+        {
+            get { return _id; }
+        }
+        public Guid AuthorId
+        {
+            get { return _authorId; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+        public bool IsDeleted
+        {
+            get { return _isDeleted; }
+            set { _isDeleted = value; }
+        }
+    }
+
+
+}
