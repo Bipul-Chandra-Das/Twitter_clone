0460fb1b110c9ffc4ed4e4d3457e08f52ba3ff24
Bipul-Chandra-Das <ayyashadnaan9@gmail.com>
2026-08-29 17:51:29 +0600

    encap_assig

--- twitterClone/twitterClone.Domain/Entities/Bookmark.cs
+++ twitterClone/twitterClone.Domain/Entities/Bookmark.cs
@@ -8,5 +8,25 @@ namespace twitterClone.Domain.Entities
 {
     internal class Bookmark
     {
+        public Guid Id { get; }
+        public Guid userId { get; }
+        public Guid postId { get; }
+        public DateTime createAt { get; }
+
+        public Bookmark(Guid userId,Guid postId)
+        {
+            if (userId == Guid.Empty)
+                throw new ArgumentException("userID required");
+            if (postId == Guid.Empty)
+                throw new ArgumentException("postId required");
+
+            this.Id = Guid.NewGuid();
+            this.postId = postId;
+            this.userId = userId;
+            createAt = DateTime.UtcNow;
+
+
+        }
+      
     }
 }
--- twitterClone/twitterClone.Domain/Entities/Notification.cs
+++ twitterClone/twitterClone.Domain/Entities/Notification.cs
@@ -8,5 +8,38 @@ namespace twitterClone.Domain.Entities
 {
     internal class Notification
     {
+        public Guid Id { get; }
+        public Guid ReciverId { get; }
+        public string Title { get; }
+        public string Message { get; }
+        public  NotificationType Type { get; }
+        public DateTime CreateAt { get; }
+
+        public Notification(Guid Id, Guid ReciverId, string Title, string message, Type NotificationType)
+        {
+            if (string.IsNullOrWhiteSpace(Title))
+            {
+                throw new ArgumentException("tittle required");
+            }
+
+            if (string.IsNullOrWhiteSpace(Message))
+            {
+                throw new ArgumentException("message required ");
+            }
+
+            Id = Guid.NewGuid();
+            this.ReciverId = ReciverId;
+            this.Title = Title;
+            this.Message = Message;
+            Type=Type;
+            this.CreateAt = DateTime.UtcNow;
+        }
+        internal enum NotificationType
+        {
+            like,
+            retweet,
+            follow,
+            message
+        }
     }
 }
